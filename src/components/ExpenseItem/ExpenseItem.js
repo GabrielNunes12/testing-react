cf0488ab4e3cd49beb4ee9f9cf34d5592ddb750a
Gabriel Nunes <prismaticnetwork12@gmail.com>
2021-11-23 07:07:42 -0300

    fix(ExpenseItem.js): adding date formatted

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -5,7 +5,9 @@ export default function ExpenseItem({expanse}) {
       {expanse.map(value => (
         <div className="expense-item" key={value.id}>
           <div>
-            {value.date.toGMTString()}
+						<div>{value.date.toLocaleString('en-US', {month: 'long'})}</div>
+						<div>{value.date.getFullYear()}</div>
+            <div>{value.date.toLocaleString('en-US', {day: '2-digit'})}</div>
           </div>
           <div className="expense-item__description">
             <h2>{value.title}</h2>
